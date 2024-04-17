08e1318d6cac320b567eade5c5b0c1aeab393553
kim <k26667210@gmail.com>
2024-04-17 11:14:34 +0900

    두번째

--- src/Ex01.java
+++ src/Ex01.java
@@ -4,6 +4,33 @@ public class Ex01 {
 	public static void main(String[] args) {
 		
 		System.out.println("나의 첫 커밋!!");
+		System.out.println("두번째 커밋!");
+		
+		// 경로 변경 명령 : cd
+		// cd 바꿀경로
+		// 접근하는 경로 확인 : ls
+		// ls
+		
+		//=====git 초기 명령어======
+		// 1. git init
+		// -->현제경로를 로컬레포지토리화 시키겠습니다 =github에서 접근 가능하게끔 허용하겠습니다.
+		// 2. 사용자 정보 등록
+		// git config --global user.name "이름"
+		// git config --global user.email "이메일"
+		// 사용자 정보 확인
+		// git config --global --list
+		// 3. 원격 저장소 등록
+		// git remote add origin 원격저장소주소
+		// 원격저장소 확인
+		// git remote -v
+		// 4. 커밋
+		// 4-1) 커밋 활성화
+		// git add . 
+		// 4-2) 커밋 진행
+		// git commit -m "커밋메시지"
+		// 5. 푸쉬 명령어
+		// git push origin master
+		
 	}
 
 }
